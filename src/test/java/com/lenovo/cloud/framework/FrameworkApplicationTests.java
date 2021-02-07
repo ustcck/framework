ed7240e56516dc9802061e9a4655a93bd55b9e6a
caokai2 <caokai2@lenovo.com>
2021-02-07 22:41:52 +0800

    first commit

---
+++ src/test/java/com/lenovo/cloud/framework/FrameworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lenovo.cloud.framework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrameworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
